e2aa26e3ee1032867e70c4ee6784ec8871a8faf2
Andre Luiz <andreplacet@gmail.com>
2020-03-30 21:13:26 -0300

    fix search email address error

--- busca_email.py
+++ busca_email.py
@@ -1,12 +1,23 @@
 import re
+
 import requests
 
-url = 'http://colegioferreiralima.com.br/contato.html'
+url = 'http://www.umc.br/grad/'
+ulr_imprensa = 'http://uniesp.edu.br/sites/suzano/imprensa.php'
 req = requests.get(url)
+req2 = requests.get(ulr_imprensa)
 
 email = re.findall(r'\w+[-\w+]+\w+@\w+.\w+.\w+', req.text)
+email2 = re.findall(r'\w+[-\w+]+\w+@\w+.\w+.\w+', req2.text)
+
+for _ in email2:
+    if _ not in email:
+        email.append(_)
 
 if email:
-    print(email)
+    print(f'\nBusca efetuada com sucesso!\n'
+          f'Imprimindo lista de e-mails encontrados na pagina web requisitada\n')
+    for _ in range(len(email)):
+        print(f'{_ + 1} - {email[_]}')
 else:
     print('E-mail ou forma de contato nao econtrados!')
